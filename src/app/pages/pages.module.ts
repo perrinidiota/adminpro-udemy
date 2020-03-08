e694014098b8df4d4ab1b5b9e39e4fde0d5ac4d5
Gabriela Gómez Ramirez <gabriela@MacBook-Air-de-Gabriela.local>
2020-03-07 23:08:47 -0300

    Input, Outpt, Viewchild

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 
 import { SharedModule } from '../shared/shared.module';
 
+import { FormsModule } from '@angular/forms';
+
+import { ChartsModule } from 'ng2-charts';
+
 import { PAGES_ROUTES } from './pages.routes';
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
 
 
 @NgModule({
@@ -15,7 +21,9 @@ import { Graficas1Component } from './graficas1/graficas1.component';
         PagesComponent,
         DashboardComponent,
         ProgressComponent,
-        Graficas1Component
+        Graficas1Component,
+        IncrementadorComponent,
+        GraficoDonaComponent
 
       ],
       exports: [
@@ -26,7 +34,10 @@ import { Graficas1Component } from './graficas1/graficas1.component';
       ],
       imports: [
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        FormsModule,
+        ChartsModule
+
       ]
     })
     export class PagesModule { }
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgressComponent implements OnInit {
 
+  progreso1: number = 20;
+  progreso2: number = 30;
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
+
+  // actualizar( event: number ) {
+
+  //   console.log('evento: ', event);
+
+  // }
+
+
 
 }
